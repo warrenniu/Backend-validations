6f6036f72bc4fd8b20b32b9ae480ba4a09745c32
warrenniu <warrenniu@gmail.com>
2021-05-31 14:00:19 -0400

    added model tests for orders

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { 
+    Order.new(
+      product_name: "Chocolate", 
+      product_count: 2,
+      customer_id: 4
+    ) 
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without a product_name" do
+    subject.product_name=nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid when length less than 1" do
+    subject.product_name=""
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without a product_count" do
+    subject.product_count=nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid when count is less than 1" do
+    subject.product_count=0
+    expect(subject).to_not be_valid
+  end
+
 end
+
