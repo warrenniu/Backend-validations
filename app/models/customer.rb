17e5e997b94c4963c22c61f0fd40c1bb8f11c75a
warrenniu <warrenniu@gmail.com>
2021-05-31 13:28:30 -0400

    added orders to customer show page

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,12 @@
 class Customer < ApplicationRecord
+    has_many :orders
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :phone,  presence: true
     validates :phone, numericality: { only_integer: true }
     validates :phone, length: { is: 10}    
+
+    
     def full_name
         "#{first_name} #{last_name}"
     end
